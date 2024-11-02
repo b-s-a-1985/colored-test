03b95a1b5cc19121a00cee5f813a445fb76ef96c
Bodnár Sándor <bodnarsa@gmail.com>
2024-11-02 20:58:47 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module colored
+
+go 1.23.2
+
+require github.com/b-s-a-1985/colored v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/b-s-a-1985/colored"
+)
+
+func main() {
+
+	fmt.Println(colored.Blue + "Blue txt")
+	fmt.Println(colored.Cyan + "Cyan txt")
+	fmt.Println(colored.Gray + "Gray txt")
+	fmt.Println(colored.Green + "Green txt")
+	fmt.Println(colored.Magenta + "Magenta txt")
+	fmt.Println(colored.White + "White txt")
+	fmt.Println(colored.Red + "Red txt")
+	fmt.Println(colored.Yellow + "Yellow txt")
+	fmt.Println()
+
+	fmt.Println(colored.BoldBlue + "Bold blue text")
+	fmt.Println(colored.BoldCyan + "Bold cyan text")
+	fmt.Println(colored.BoldGray + "Bold gray text")
+	fmt.Println(colored.Green + "Bold green text")
+	fmt.Println(colored.BoldMagenta + "Bold magenta text")
+	fmt.Println(colored.BoldWhite + "Bold white text")
+	fmt.Println(colored.BoldYellow + "Bold yellow text" + colored.Reset)
+	fmt.Println()
+}
